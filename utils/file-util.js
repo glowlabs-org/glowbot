fab8001d6e821c120c2a7140bc0f3b4b24aa4dd7
Shane Feehily <shane.feehily@zilliant.com>
2025-02-23 16:53:28 +0100

    Enable multi channel youtube monitoring

--- utils/file-util.js
+++ utils/file-util.js
@@ -4,8 +4,6 @@ const path = require('path');
 async function getFileData(dbFilePath) {
     let data = []
 
-    console.log(dbFilePath)
-    console.log(fs.existsSync(path.join(__dirname, '../db')))
     // create db folder if it doesn't exist already
     if (!fs.existsSync(dbFilePath)) {
         fs.mkdirSync(path.join(__dirname, '../db'), { recursive: true });
